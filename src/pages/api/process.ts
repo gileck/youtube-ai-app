ecf3ebd9931a244740b9fb96a0c69dbd2a1d7bb3
Gil <gil@Gils-MacBook-Pro.local>
2025-04-27 12:03:58 +0300

    commit

--- src/pages/api/process.ts
+++ src/pages/api/process.ts
@@ -7,9 +7,13 @@ export default async function handler(
 ) {
 
   console.log('API request:', req.body);
-  
+
   const response = await processApiCall(req);
 
   // console.log('API response:', response);
   return res.status(200).json(response);
 }
+
+export const config = {
+  maxDuration: 60,
+};
