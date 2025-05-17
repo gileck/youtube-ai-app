8b42d27c7ed59fbbdd25cd9dee4cbe5a333d5aad
Gil <gil@Gils-MacBook-Pro.local>
2025-05-17 12:25:37 +0300

    commit

--- src/client/components/NavLinks.tsx
+++ src/client/components/NavLinks.tsx
@@ -1,9 +1,7 @@
 import { NavItem } from './layout/types';
 import HomeIcon from '@mui/icons-material/Home';
 import ChatIcon from '@mui/icons-material/Chat';
-import FolderIcon from '@mui/icons-material/Folder';
 import SettingsIcon from '@mui/icons-material/Settings';
-import InsightsIcon from '@mui/icons-material/Insights';
 
 export const navItems: NavItem[] = [
   { path: '/', label: 'Home', icon: <HomeIcon /> },
